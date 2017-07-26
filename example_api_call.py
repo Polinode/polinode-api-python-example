5093c814557eb15b7e9fc253560a80cd2d1c783c
Andrew Pitts <andrewjohnpitts@gmail.com>
2017-07-26 12:54:19 +1000

    Update for v2 of API

--- example_api_call.py
+++ example_api_call.py
@@ -79,7 +79,7 @@ exampleNetworkData = {
 }
 
 #Create a network
-r = requests.post('https://www.polinode.com/api/v2/networks', auth=(username, password), data = {'name': 'My new network', 'networkJSON': json.dumps(exampleNetworkData), 'status': 'Public', 'fileType': 'JSON', 'originalFileType': 'JSON', 'isDirected': 'true', 'description': 'An example network created via the Polinode API'}) #name is required, networkJSON is required, status is optinal and defaults to public, fileType is optional and defaults to JSON, originalFileType is optional and defaults to Excel, isDirected is optional and defaults to true, description is optional and defautls to no description.
+r = requests.post('https://wwww.polinode.com/api/v2/networks', auth=(username, password), json = {'name': 'My new network', 'networkJSON': exampleNetworkData, 'status': 'Public', 'fileType': 'JSON', 'originalFileType': 'JSON', 'isDirected': 'true', 'description': 'An example network created via the Polinode API'}) #name is required, networkJSON is required, status is optinal and defaults to public, fileType is optional and defaults to JSON, originalFileType is optional and defaults to Excel, isDirected is optional and defaults to true, description is optional and defautls to no description.
 network = r.json()
 networkId = network['_id']
 print('Summary of network created:')
@@ -87,25 +87,25 @@ pprint.pprint(network, indent=2)
 
 #Edit the network we just created. We will change the name of the network and change one attribute value
 exampleNetworkData['nodes'][0]['attributes']['Example Numerical Attribute Name'] = 25;
-r = requests.put('https://www.polinode.com/api/v2/networks/' + networkId, auth=(username, password), data = {'name': 'My new network after edits', 'networkJSON': json.dumps(exampleNetworkData)})
+r = requests.put('https://wwww.polinode.com/api/v2/networks/' + networkId, auth=(username, password), json = {'name': 'My new network after edits', 'networkJSON': exampleNetworkData})
 network = r.json()
 print('Summary of updated network:')
 pprint.pprint(network, indent=2)
 
 #Example of retrieving a summary of all networks for a user
-r = requests.get('https://www.polinode.com/api/v2/networks', auth=(username, password))
+r = requests.get('https://wwww.polinode.com/api/v2/networks', auth=(username, password))
 networks = r.json()
 print('Summary of networks')
 pprint.pprint(networks, indent=2)
 
 #Example of retrieving a specific network for a user. Authentication is not required for this action if this is a public network.
-r = requests.get('https://www.polinode.com/api/v2/networks/' + networkId, auth=(username, password))
+r = requests.get('https://wwww.polinode.com/api/v2/networks/' + networkId, auth=(username, password))
 network = r.json()
 print('Summary for a single network')
 pprint.pprint(network, indent=2)
 
 #Delete the network that we created. Comment this section out to not delete the created network, i.e. to view it in the application
-r = requests.delete('https://www.polinode.com/api/v2/networks/' + networkId, auth=(username, password))
+r = requests.delete('https://wwww.polinode.com/api/v2/networks/' + networkId, auth=(username, password))
 network = r.json()
 print('Network deleted')
 pprint.pprint(network, indent=2)
